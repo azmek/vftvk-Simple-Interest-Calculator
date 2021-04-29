62e8ec374c88ada1fb56c0c3505b6a4387429f7a
azmek <azmek@users.noreply.github.com>
2021-04-29 13:29:03 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 function compute()
 {
     var amount = document.getElementById("principal").value;
+    if (amount <= 0) {
+        alert("Please enter a number and a positive value")
+        frameElement.focus();
+        return false;
+    }
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value
@@ -9,7 +14,7 @@ function compute()
 
     var year = new Date().getFullYear()+parseInt(years);
     
-
+  
     document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>";
 }
 function updateRate() 
